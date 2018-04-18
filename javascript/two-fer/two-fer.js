01fdd3633c088a21cc8b01baafbb56269b40c564
Ilana Davis <ilana@ilanadavis.com>
2018-04-18 22:39:08 +0000

    exercism complete with conditional statements

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,12 +1,16 @@
 var TwoFer = function () {};
 
 TwoFer.prototype.twoFer = function (who) {
-  // your code goes here
-  // You will have to use the parameter who
-  // in some way. In this example, it is just
-  // returned, but your solution will have to
-  // use a conditional.
-  return who;
+
+  if (who === 'Alice') {
+    return 'One for Alice, one for me.';
+  }
+  else if (who === 'Bob') {
+    return 'One for Bob, one for me.';
+  }
+  else {
+    return 'One for you, one for me.';
+  }
 };
 
 module.exports = TwoFer;
