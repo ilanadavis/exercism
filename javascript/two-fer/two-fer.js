b78105b7e456384f639c81f7a7d1340f2c294234
Ilana Davis <ilana@ilanadavis.com>
2018-04-18 22:48:35 +0000

    Alternate using ES2015 solution

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,16 +1,7 @@
 var TwoFer = function () {};
 
 TwoFer.prototype.twoFer = function (who) {
-
-  if (who === 'Alice') {
-    return 'One for Alice, one for me.';
-  }
-  else if (who === 'Bob') {
-    return 'One for Bob, one for me.';
-  }
-  else {
-    return 'One for you, one for me.';
-  }
+  return `One for ${who || 'you'}, one for me.`;
 };
 
 module.exports = TwoFer;
