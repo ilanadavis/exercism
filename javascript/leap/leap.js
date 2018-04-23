8fec50d7bf34a0d77ef89444b08eab23497bcd1b
Ilana Davis <ilana@ilanadavis.com>
2018-04-23 23:09:36 +0000

    Add formula for isleap. All test pass

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -8,7 +8,13 @@ var Year = function (year) {
 };
 
 Year.prototype.isLeap = function () {
-  return false
+  if (((this.year % 4 == 0) && (this.year % 100 != 0)) 
+  || (this.year % 400 == 0)) {
+    return true;
+  }
+  else {
+    return false;
+  }
 };
 
 module.exports = Year;
